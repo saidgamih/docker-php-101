66006550d47ac532eb9caad122396e09a3216379
Said Gamih <said.gamih@gmail.com>
2023-05-19 16:14:27 +0100

    Adding content to front page 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return [
-            'message' => 'Hello'
-        ];
+        return view('index');
     }
 }
